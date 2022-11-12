0f0658c1fa0657e13da5adae313802096d91a9b7
Ayanokoujifl <guleite3@gmail.com>
2022-11-12 18:29:17 -0300

    701 pokemons with gif

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ export const fetchPokemom = () => {
 
   const pokemomPromisses = []
 
-  for (let i = 1; i <= 905; i++) {
+  for (let i = 1; i <= 721; i++) {
     pokemomPromisses.push(
       fetch(getPokemomUrl(i)).then((response) => response.json())
     )
